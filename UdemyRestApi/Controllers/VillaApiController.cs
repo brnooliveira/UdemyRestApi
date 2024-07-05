eafbbd1284a81f86596e2d16b4dc66ef513043bf
breno <breno@DESKTOP-3JNC0II>
2024-07-05 00:07:11 -0300

    Adicionar arquivos de projeto.

---
+++ UdemyRestApi/Controllers/VillaApiController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyRestApi.Models;
+
+namespace UdemyRestApi.Controllers
+{
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return
+            [
+                new() {Id = 1, Name = "Pool Villa" },
+                new() {Id = 2, Name = "Beach Villa" }
+            ];
+        }
+    }
+}
